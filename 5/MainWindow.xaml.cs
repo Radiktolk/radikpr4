e1360e5c9c4d83b4c6dcc68c810fda721232c873
radmir <Milk>
2023-12-07 15:20:10 +0300

    radik

---
+++ 5/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void compute()
+        {
+
+            try
+            {
+
+                double p = double.Parse(P.Text);
+
+                if (0 < p && p < 50) { }
+
+                else { throw new ArgumentOutOfRangeException(); }
+
+
+                double q = 1 + p / 100;
+
+
+                double S_(int n)
+                {
+                    return (10 * (1 - Math.Pow(q, n)) / (1 - q));
+                }
+
+
+                int k = 0;
+
+                for (int i = 1; S_(i) <= 200; i++)
+                {
+                    k = i + 1;
+                }
+
+                K.Content = $"{k}";
+
+                S.Content = $"{S_(k + 1)}";
+
+
+
+            }
+
+            catch
+            {
+                K.Content = "-";
+                S.Content = "-";
+            }
+
+        }
+
+
+        private void P_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            compute();
+        }
+    }
+}
---
+++ 6/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void compute()
+        {
+
+            try
+            {
+
+                int n = int.Parse(N.Text);
+
+                if (n > 0) { }
+
+                else { throw new ArgumentOutOfRangeException(); }
+
+
+                bool result = false;
+
+                foreach (char symbol in N.Text)
+                {
+                    if (int.Parse($"{symbol}") % 2 == 0)
+                    {
+                        result = true;
+                    }
+                }
+
+                Result.Content = $"{result}";
+
+            }
+
+            catch
+            {
+                Result.Content = "-";
+            }
+
+        }
+
+
+        private void N_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            compute();
+        }
+    }
+}
